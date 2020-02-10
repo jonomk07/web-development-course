a00299c39246798232306a7ae850e0fae26d3e67
Jonathan Muamba <jonathan.muamba@wundermanthompson.com>
2020-02-10 14:33:08 +0200

    Watching: Do you know how to use the conditional operator?

--- process/js/script.js
+++ process/js/script.js
@@ -1,27 +1,28 @@
-(function(howMany, colors) {
+(function (howMany, colors) {
 
-  var colorAmt = colors.length;
-  var currColor = 0;
-  var myElement;
-  var myNode = document.querySelector('.boxes');
+    var colorAmt = colors.length;
+    var currColor = 0;
+    var myElement;
+    var myNode = document.querySelector('.boxes');
 
-  for (var i = 0; i < howMany; i++) {
+    for (var i = 0; i < howMany; i++) {
 
-    myElement = document.createElement('div');
-    myElement.className = 'box';
-    myElement.style = 'background-color: ' + colors[currColor];
-    myNode.appendChild(myElement);
+        myElement = document.createElement('div');
+        myElement.className = 'box';
+        myElement.style = 'background-color: ' + colors[currColor];
+        myNode.appendChild(myElement);
 
-    if (currColor === colorAmt-1) {
-      currColor = 0;
-    } else {
-      currColor++;
+//        if (currColor === colorAmt - 1) {
+//            currColor = 0;
+//        } else {
+//            currColor++;
+//        }
+        currColor === colorAmt - 1 ? currColor = 0 : currColor++;
     }
-  }
 
-  myNode.addEventListener('click', function(e) {
-    e.target.parentNode.removeChild(e.target);
-  }, false);
+    myNode.addEventListener('click', function (e) {
+        e.target.parentNode.removeChild(e.target);
+    }, false);
 
 })(20, [
   '#C94C24', //orange
