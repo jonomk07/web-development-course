4f36f30811d2a5b6237f4819193b16a9ae377762
Jonathan Muamba <jonathan.muamba@wundermanthompson.com>
2020-02-10 11:37:46 +0200

    Are you comfortable using jQuery?

--- process/js/script.js
+++ process/js/script.js
@@ -1,32 +1,14 @@
-var artists, calendar;
-
-function loadData(url) {
-  return new Promise(function (resolve, reject) {
-    var req = new XMLHttpRequest();
-    req.open('GET', url);
-    req.onload = function () {
-      if (req.status == 200) {
-        console.log('getting ' + url);
-        resolve(JSON.parse(req.response))
-      } else {
-        reject(Error('Problem loading data'))
-      }
-    } // onload callback
-    req.send();
-  });
-}
-
-calendar = loadData('js/data_schedule.json');
-artists = loadData('js/data_artists.json');
-
-document.querySelector('.load-info')
-  .addEventListener('click', function () {
-    calendar.then(function (data) {
-      console.log(data);
-      return artists;
-    }, function (error) {
-      console.log('Problem', error);
-    }).then(function (otherData) {
-      console.log(otherData);
-    });
-  });
+$(function(){
+    $('.load-info').on('click',function(){
+       $.ajax({
+           method: 'GET',
+           url: 'js/data_artists.json',
+           dataType: 'json'
+       }).done(function(data){
+          $.each(data.artists, function(key,val){
+              $('.info-artists')
+              .append('<li>' + val.name + '<li>')
+          }) //each
+       });//ajax done
+    });// click event
+});// jQuery Ready
